6f6824af9cc79cd9f974caae222134d3d2c9e786
mohannadmaklad <mshm.maklad@gmail.com>
2022-08-06 18:26:05 +0200

    raindrops solution

---
+++ cpp/raindrops/raindrops.h
@@ -0,0 +1,10 @@
+#if !defined(RAINDROPS_H)
+#define RAINDROPS_H
+
+#include <string>
+
+namespace raindrops {
+    std::string convert(unsigned int n);
+}  // namespace raindrops
+
+#endif // RAINDROPS_H
