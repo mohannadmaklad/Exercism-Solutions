ac95635d064e2e241dead0c3715e454a5e5432e8
mohannadmaklad <mshm.maklad@gmail.com>
2022-08-07 21:25:13 +0200

    two_fer solution

---
+++ cpp/two-fer/build/CMakeFiles/two-fer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/two-fer.dir/test/tests-main.cpp.obj"
+  "CMakeFiles/two-fer.dir/two_fer.cpp.obj"
+  "CMakeFiles/two-fer.dir/two_fer_test.cpp.obj"
+  "libtwo-fer.dll.a"
+  "two-fer.exe"
+  "two-fer.exe.manifest"
+  "two-fer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/two-fer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/two-fer/two_fer.cpp
@@ -0,0 +1,10 @@
+#include "two_fer.h"
+
+namespace two_fer
+{
+    std::string two_fer(std::string const &name)
+    {
+            return "One for " + name + ", one for me.";
+    }
+} // namespace two_fer
+
